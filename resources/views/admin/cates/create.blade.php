abb6685c81f158fe9bb09a4eb5ed15121debdfe4
smokinglife <978952042@qq.com>
2019-03-01 16:46:14 +0800

    增加了后台友情链接模块

---
+++ resources/views/admin/cates/create.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.layout.index')
+
+@section('content')
+				
+		<p></p>
+		<div class="col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                                    
+        <div class="card-title">
+            <h2><div class="title">分类管理</div></h2>
+        </div>
+    </div>
+
+	<form class="form-horizontal" action="/admin/cates" method="post">
+		{{ csrf_field() }}
+	    <div class="form-group">
+	        <label class="col-sm-2 control-label">分类</label>
+	        <div class="col-sm-10" style="width:600px;">
+	            <input type="text" class="form-control" placeholder="分类名称" name="cname">
+	        </div>
+	    </div>
+
+	    <div class="form-group">
+	        <label class="col-sm-2 control-label">所属分类</label>
+	        <div class="col-sm-10" style="width:600px;">
+	            <select name="pid" class="form-control" class="col-sm-10" style="height:34px;border-radius:;">
+	                <option value="0">--请选择--</option>
+	                @foreach($cates_data as $k=>$v)
+	                <option value="{{ $v->id }}" @if($id == $v->id) selected @endif>{{ $v->cname }}</option>
+	                @endforeach
+	            </select>
+	        </div>
+	    </div>    
+	    
+	    <div class="form-group">
+	        <div class="col-sm-offset-2 col-sm-10">
+	            <button type="submit" class="btn btn-default btn btn-info">提交</button>
+	            <button type="reset" class="btn btn-default">重置</button>
+	        </div>
+	    </div>
+	</form>
+	        </div>
+	    </div>
+	</div>
+
+@endsection
---
+++ resources/views/admin/link/index.blade.php
@@ -0,0 +1,86 @@
+@extends('admin.layout.index')
+
+
+@section('content')
+<div class="panel panel-default">
+	<div class="panel-heading">
+	    <h2>申请列表</h2>
+	</div>
+	<div class="panel-body">
+    <div id="dataTables-example_wrapper" class="dataTables_wrapper form-inline" role="grid">
+    <form action="/admin/link" method="get">
+	    <div class="row">
+	      <div class="col-sm-6">
+	        <div class="dataTables_length" id="dataTables-example_length">
+	          <label>显示
+	            <select name="count" aria-controls="dataTables-example" class="form-control input-sm">
+	              <option value="5" @if(isset($request['count']) && $request['count'] == 10) selected @endif>5</option>
+	              <option value="15" @if(isset($request['count']) && $request['count'] == 15) selected @endif>15</option>
+	              <option value="25" @if(isset($request['count']) && $request['count'] == 20) selected @endif>25</option>
+	              <option value="35" @if(isset($request['count']) && $request['count'] == 50) selected @endif>35</option></select>&nbsp;条</label>
+	        </div>
+	      </div>
+	      <div class="col-sm-6">
+	        <div id="dataTables-example_filter" class="dataTables_filter">
+	          <label style="float:right;">关键词:
+	            <input type="text" name="search" class="form-control input-sm" aria-controls="dataTables-example" value="{{ $request['search'] or ''}}">
+	            <input type="submit" value="搜索" class="btn btn-info"></label></div>
+	      </div>
+	    </div>
+	</form>
+    <table class="table table-striped table-bordered table-hover dataTable no-footer " id="dataTables-example" aria-describedby="dataTables-example_info">
+    <thead>
+        <div class="table-responsive">
+		  <tr role="row">
+		    <th>ID</th>
+		    <th>网站名称</th>
+		    <th>网址</th>
+		    <th>电子邮箱</th>
+		   	<th>网站介绍</th>
+		    <th  class="text-center">审核</th>
+		</tr>
+    </thead>
+    <tbody> 
+    @foreach($data as $key=>$v)    
+    <tr style="background:none;">
+        <td>{{$v->id}}</td>
+        <td>{{$v->link_name}}</td>
+        <td>{{$v->link_adr}}</td>
+        <td>{{$v->link_email}}</td>
+        <td>
+        	<abbr title="{{$v->link_des}}">
+        		<p style="width: 100px;overflow: hidden;text-overflow:ellipsis;white-space: nowrap; ">{{$v->link_des}}</p>
+       		 </abbr>
+        </td>
+        <td>
+        	@if($v->link_agree == 0) 
+        	<a href="/admin/link/{{$v->id}}/edit" class="btn btn-success ">通过</a>
+        	<form action="/admin/link/{{$v->id}}" method="post" style="display: inline-block;">
+        		{{ csrf_field() }}
+        		{{ method_field('DELETE')}}
+				<input type="submit" value="删除" class="btn btn-danger">
+        		
+        	</form>
+        	@else
+        	<a href="/admin/link/{{$v->id}}/edits" class="btn btn-success" disabled="disabled">通过审核</a>
+        	@endif
+        </td>
+    </tr>
+    @endforeach
+	</tbody>
+    </table>
+   <div class="table-responsive">
+	  <div class="row">
+	    <div class="col-sm-12">
+	    	<div style="float:right;">
+	    		{{ $data->appends($request)->links() }}
+	    	</div>
+	     </div>
+	    <div class="col-sm-6">
+	      <div class="dataTables_paginate paging_simple_numbers" id="dataTables-example_paginate">
+	      </div>
+	    </div>
+	  </div>
+	</div>
+</div>
+@endsection
