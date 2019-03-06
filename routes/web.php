e13df4a3f59e8fd30427a7f1cf5434f4a54e382c
lamp21 <841289088@qq.com>
2019-03-06 09:16:20 +0800

    as 修改了前台模板

--- routes/web.php
+++ routes/web.php
@@ -18,60 +18,33 @@ Route::get('/', function () {
  
 加载后台模板
 */
-Route::get('admin','Admin\IndexController@index');
 
-Route::get('admin/users/setdata','Admin\UserController@setdata');
+// 子分类
+Route::get('admin/cates/create/{id}','Admin\CatesController@create');
 
-//用户管理
-Route::resource('admin/users','Admin\UserController');
+// 添加 分类
+Route::get('admin/cates/create','Admin\CatesController@create');
 
+// 查看 子分类
+Route::get('admin/cates/{id}','Admin\CatesController@index');
 
-
-
-
-
-
-
-Route::get('admin/users/setdata','Admin\UserController@setdata');
-
-
-//用户管理
-Route::resource('admin/users','Admin\UserController');
-
-//子分类
-Route::get('admin/cates/create/{id}','Admin\catesController@create');
-
-
-//分类管理
+// 分类管理
 Route::resource('admin/cates','Admin\CatesController');
 
-
 // 前台 分类
 Route::resource('home/index','Home\IndexController');
 
-
-
-
-
-
-
-// 前台 分类
-Route::resource('home/index','Home\IndexController');
-
-
-
-
-
-
+// 后台 登录 测试
 Route::prefix('admin')->namespace('Admin')->group(function () {
-    //后台首页
+    // 后台 首页
     $this->get('login', 'LoginController@showLoginForm')->name('admin.login');
     $this->post('login', 'LoginController@login');
     $this->post('logout', 'LoginController@logout')->name('admin.logout');
 });
 
+// 后台 登录 测试
 Route::prefix('admin')->namespace('Admin')->group(function () {
-    //后台首页
+    // 后台 首页
     $this->get('login', 'LoginController@showLoginForm')->name('admin.login');
     $this->post('login', 'LoginController@login');
     $this->post('logout', 'LoginController@logout')->name('admin.logout');
@@ -80,17 +53,27 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
     });
 });
 
-// 后台登录
+// 后台 登录 测试
+Route::group(['namespace' => 'Auth','prefix'=>'auth'],function(){ 
+    route::get('/login','AuthController@login'); 
+    route::get('/logout','AuthController@getlogout'); 
+    route::post('/login','AuthController@auth'); 
+});
+
+// 后台 登录
 Route::resource('admin/login','Admin\LoginController');
 
-// gg
-// Route::get('admin/advert/create','Admin\catesController@create');
+// 广告 列表
+Route::get('admin/advert/create','Admin\catesController@create');
 
 // 广告
-
 Route::resource('admin/advert','Admin\AdvertController');
 
+// 前台 广告 申请
+Route::resource('home/create','Home\AdvertController');
 
+// 前台 广告 申请
+Route::resource('home/advert','Home\AdvertController');
 
 
 
@@ -135,31 +118,80 @@ Route::resource('admin/advert','Admin\AdvertController');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Route::get('home/login','Home\IndexController@login');
-
-Route::post('home/index/dologin','Home\IndexController@dologin');
-
-//前台  注册
-Route::resource('home/register','Home\RegisterController');
-// 前台 分类
-Route::resource('home/index','Home\IndexController');
-
-
-//友情链接管理
+// 友情链接管理
 Route::resource('admin/link','Admin\LinkController');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 后台 模板
+Route::get('admin','Admin\IndexController@index');
+
+// 后台 测试
+Route::get('admin/users/setdata','Admin\UserController@setdata');
+
+// 前台 登录
+Route::get('home/login','Home\IndexController@login');
+
+// 前台 注册
+Route::post('home/index/dologin','Home\IndexController@dologin');
+
+// 前台  注册
+Route::resource('home/register','Home\RegisterController');
+
+// 用户 管理
+Route::resource('admin/users','Admin\UserController');
